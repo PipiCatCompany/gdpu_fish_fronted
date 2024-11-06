977b83d2b0d8096602398ebabf2c9bb31d40ad1b
上术 <10920520+hnitro@user.noreply.gitee.com>
2024-11-06 22:45:48 +0800

    feature: 商品预览前端 / 学号绑定

--- api/user.js
+++ api/user.js
@@ -1,5 +1,23 @@
 import request from "@/request/index.js";
 
+// utils functions 
+// getStorageSync 不加try-catch 小程序会crash storage - 估计强制异步
+export const GetToken = () => {
+	try {  
+	    return uni.getStorageSync("token") 
+	} catch (error) {  
+	    console.log(error)  
+	}
+}
+
+export const GetUser = () => {
+	try {
+	    return  uni.getStorageSync("user")
+	} catch (error) {  
+	    console.log(error)  
+	}
+}
+
 // wx.login获取临时code获取openid
 export const GetOpenId = (code) => {
 	return request({
@@ -12,5 +30,14 @@ export const LoginByOpenId = (openid) => {
 	return request({
 		url: `/login_openid?openid=${openid}`,
 		method: 'get',
+	})
+}
+
+export const updateUserStudentCode = (data,token) => { 
+	return request({
+		url: '/user/studentcode',
+		method: 'put',
+		data: data,
+		token: token
 	})
 }
