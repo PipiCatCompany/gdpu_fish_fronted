791b689960ddccc70923e81e16561fb8f9bba598
Hnitrogen <1791422575@qq.com>
2024-11-11 23:41:39 +0800

    Delete unpackage/dist directory

--- unpackage/dist/dev/mp-weixin/common/assets.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const _imports_0$1 = "/static/logo.png";
-const _imports_0 = "/static/tabber/chat_cur.png";
-const _imports_1 = "/static/tabber/about_cur.png";
-exports._imports_0 = _imports_0$1;
-exports._imports_0$1 = _imports_0;
-exports._imports_1 = _imports_1;
